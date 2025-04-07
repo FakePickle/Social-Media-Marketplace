1acb10dc68f765dd770ca63160b77b1dcd329656
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2025-04-07 06:49:54 +0530

    fix: fix import statements

--- frontend/rivr/src/pages/AdminDashboard.jsx
+++ frontend/rivr/src/pages/AdminDashboard.jsx
@@ -1,17 +1,17 @@
 import {React, useState} from 'react'
-import TopNav from '../Components/TopNav'
-import AdminSideBar from '../Components/AdminSideBar'
-import UserManagement from '../Components/UserManagement';
-import DocumentVerifications from '../Components/DocumentVerifications';
-import SecurityAudits from '../Components/SecurityAudits';
-import Settings from '../Components/Settings';
-import Account from '../Components/Account';
+import TopNav from '../components/TopNav'
+import AdminSideBar from '../components/AdminSideBar'
+import UserManagement from '../components/UserManagement';
+import DocumentVerifications from '../components/DocumentVerifications';
+import SecurityAudits from '../components/SecurityAudits';
+import Settings from '../components/Settings';
+import Account from '../components/Account';
 
 function AdminDashboard({userList}) {
   const [currentTab, setCurrentTab] = useState("User Management");
   const [expanded, setExpanded] = useState(true);
-  
-  
+
+
   return (
     <div style={{backgroundColor:"#1B263B", overflow: "hidden"}}>
     <AdminSideBar  currentTab={currentTab} setCurrentTab={setCurrentTab} expanded={expanded} setExpanded={setExpanded}/>
